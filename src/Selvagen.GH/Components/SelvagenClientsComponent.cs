10cdd5ec2cfc05107a2539b0b46730b901137169
Daniel Locatelli <daniellocatelli@proton.me>
2026-05-17 20:45:33 +0200

    refactor(gh): port SelvagenClientsComponent to selectable base with inline dropdown

--- src/Selvagen.GH/Components/SelvagenClientsComponent.cs
+++ src/Selvagen.GH/Components/SelvagenClientsComponent.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Grasshopper.Kernel;
 using Selvagen.Core.Api;
@@ -8,71 +6,24 @@ using Selvagen.Core.Models;
 
 namespace Selvagen.GH.Components
 {
-    public class SelvagenClientsComponent : GH_Component
+    public class SelvagenClientsComponent : SelvagenSelectableComponentBase<FirmInfo>
     {
-        private FirmInfo[] _cachedClients;
-        private bool _refreshWasTrue;
-
         public SelvagenClientsComponent()
             : base("List Clients", "SvClients",
-                "List clients of the firm.",
-                "Selvagen", "Data")
+                "List clients of the firm. Pick one from the inline dropdown to feed downstream components.",
+                "Data")
         { }
 
         public override Guid ComponentGuid => new Guid("F23D9E81-A7C2-4B1D-8F9E-3D4C5B6A7E8F");
 
-        protected override void RegisterInputParams(GH_InputParamManager pManager)
-        {
-            pManager.AddBooleanParameter("Refresh", "R", "Force a re-fetch", GH_ParamAccess.item, false);
-        }
+        protected override object[] CaptureInputs(IGH_DataAccess da) => new object[0];
 
-        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
-        {
-            pManager.AddTextParameter("IDs", "ID", "Client IDs", GH_ParamAccess.list);
-            pManager.AddTextParameter("Names", "N", "Client names", GH_ParamAccess.list);
-        }
+        protected override Task<FirmInfo[]> FetchAsync(SelvagenClient client, object[] inputs)
+            => client.ListClientsAsync();
 
-        protected override void SolveInstance(IGH_DataAccess DA)
-        {
-            bool refresh = false;
-            DA.GetData(0, ref refresh);
+        protected override string GetId(FirmInfo item) => item.Id;
+        protected override string GetDisplayName(FirmInfo item) => item.LegalName;
 
-            var client = SessionManager.Current;
-            if (client == null)
-            {
-                AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Not logged in. Place a Login component first.");
-                DA.SetDataList(0, new List<string>());
-                DA.SetDataList(1, new List<string>());
-                return;
-            }
-
-            bool needsFetch = _cachedClients == null || (refresh && !_refreshWasTrue);
-            _refreshWasTrue = refresh;
-
-            if (needsFetch)
-            {
-                try
-                {
-                    PluginLogger.Log("SelvagenClientsComponent: Fetching clients...");
-                    _cachedClients = Task.Run(() => client.ListClientsAsync()).GetAwaiter().GetResult();
-                    PluginLogger.Log($"SelvagenClientsComponent: Found {_cachedClients.Length} clients.");
-                }
-                catch (Exception ex)
-                {
-                    string errorMsg = ex.InnerException?.Message ?? ex.Message;
-                    PluginLogger.Log($"SelvagenClientsComponent Error: {errorMsg}");
-                    AddRuntimeMessage(GH_RuntimeMessageLevel.Error, errorMsg);
-                    DA.SetDataList(0, new List<string>());
-                    DA.SetDataList(1, new List<string>());
-                    return;
-                }
-            }
-
-            DA.SetDataList(0, _cachedClients.Select(c => c.Id).ToList());
-            DA.SetDataList(1, _cachedClients.Select(c => c.LegalName).ToList());
-        }
-
-        public override GH_Exposure Exposure => GH_Exposure.primary;
         protected override System.Drawing.Bitmap Icon => IconLoader.Load("Clients");
     }
 }
